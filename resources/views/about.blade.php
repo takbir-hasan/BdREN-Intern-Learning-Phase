cec538c75e933b3be6deb16c639d2fd2f7c7db08
takbir-hasan <takbirhasan274@gmail.com>
2026-02-10 16:25:17 +0600

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+        <x-slot:heading>
+            About Page
+        </x-slot:heading>
+
+        <h1>About Us</h1>
+        <p>This is the about page.</p>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+        <x-slot:heading>
+            Contact Page
+        </x-slot:heading>
+
+        <h1>Contact Us</h1>
+        <p>This is the contact page.</p>
+</x-layout>
